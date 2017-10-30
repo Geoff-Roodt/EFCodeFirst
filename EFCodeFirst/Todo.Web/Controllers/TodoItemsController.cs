a8a9d0fbafbe68a8a9ad0a2036a60a4ca32e106c
Geoff Roodt <geoffr@ezitracker.co.nz>
2017-10-30 13:50:06 +1300

    End point

---
+++ EFCodeFirst/Todo.Web/Controllers/TodoItemsController.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Web.Http;
+using Todo.Context;
+using Todo.Web.Service.Interfaces;
+using Todo.Web.Service.Services;
+
+namespace Todo.Web.Controllers
+{
+    public class TodoItemsController : ApiController
+    {
+        private IService<TodoItem> Service { get; set; }
+
+        public TodoItemsController()
+        {
+            TodoContext context = new TodoContext();
+            Service = new Service<TodoItem>(context);
+        }
+
+
+        [HttpGet]
+        [Route("todoitems")]
+        public IQueryable<TodoItem> Get()
+        {
+            return Service.GetAll().AsQueryable();
+        }
+
+
+    }
+}
--- EFCodeFirst/Todo.Web/Startup.cs
+++ EFCodeFirst/Todo.Web/Startup.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Owin;
+﻿using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartup(typeof(Todo.Web.Startup))]
@@ -14,5 +11,6 @@ namespace Todo.Web
         {
             ConfigureAuth(app);
         }
+
     }
 }
